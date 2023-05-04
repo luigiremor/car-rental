b6534dd39d3b456ca47989b58d73989fb5486ecc
Luigi Remor <luigiremor@gmail.com>
2023-05-04 15:22:00 -0300

    WIP: main

---
+++ main.py
@@ -0,0 +1,9 @@
+from controller.aplicacaocontroller import AplicacaoController
+
+
+def main():
+    controller = AplicacaoController()
+    controller.handle_menu_login()
+
+if __name__ == "__main__":
+    main()
